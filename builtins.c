f4bd5bc783c6ace811ad5aea691432de24189d74
Salim Hmimi <shmimi@c1r7p8.1337.ma>
2023-07-23 10:20:23 +0100

    leaks h done

--- builtins.c
+++ builtins.c
@@ -84,7 +84,7 @@ void my_echo(char **cmd)
     if (flag && !blank)
     {
         printf("\n");
-        return ;
+        return;
     }
     if (!flag && cmd[0])
     {
@@ -141,7 +141,7 @@ int my_cd(char *path, t_list *my_env, t_pipes_n_redirection *pipes_n_redirection
     else
     {
         ft_printf("bash: cd: HOME not set\n");
-     	global_struct.g_exit_status = 1;
+        global_struct.g_exit_status = 1;
         return global_struct.g_exit_status;
     }
     return global_struct.g_exit_status;
@@ -153,7 +153,7 @@ void my_pwd(t_list *my_env)
     if (getcwd(pwd, sizeof(pwd)) == NULL)
     {
         perror("getcwd");
-       global_struct.g_exit_status = 1;
+        global_struct.g_exit_status = 1;
         return;
     }
     printf("%s\n", pwd);
@@ -221,6 +221,27 @@ void new_env(t_list **my_env)
     }
 }
 
+int check_myexport(char *cmd, int i)
+{
+    if ((cmd[i] >= 32 && cmd[i] <= 47) || (cmd[i] >= 58 && cmd[i] <= 60) || (cmd[i] >= 63 && cmd[i] <= 64) || (cmd[i] >= 91 && cmd[i] <= 94) || (cmd[i] >= 123 && cmd[i] <= 126) || cmd[i] == 96)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int get_equal_pos(char *key)
+{
+    int i = 0;
+    while (key[i])
+    {
+        if (key[i] == '=')
+            return i + 1;
+        i++;
+    }
+    return ft_strlen(key);
+}
+
 void myexport(char **cmd, t_list *my_env, t_pipes_n_redirection *pipes_n_redirection)
 {
     int i = 0;
@@ -272,10 +293,6 @@ void myexport(char **cmd, t_list *my_env, t_pipes_n_redirection *pipes_n_redirec
             else
                 printf("declare -x %s\n", my_export->key);
             current = current->next;
-            // free(my_export->key_exp);
-            // free(my_export->all);
-            // free(my_export->key);
-            // free(my_export->value);
         }
     }
     free(my_export);
@@ -285,11 +302,42 @@ void myexport(char **cmd, t_list *my_env, t_pipes_n_redirection *pipes_n_redirec
 
     while (cmd[i])
     {
-        // printf("%s\n", cmd[i]);
+        key = ft_substr(cmd[i], 0, get_equal_pos(cmd[i]));
+        while (key[j] && key)
+        {
+            if (check_myexport(key, j))
+            {
+                ft_printf("bash: not a valid identifiers\n");
+                global_struct.g_exit_status = 1;
+                pipes_n_redirection->exit_builtin = 1;
+                return;
+            }
+            j++;
+        }
+        j = 0;
+        i++;
+        free(key);
+    }
+
+    key = NULL;
+    i = 0;
+    j = 0;
+    while (cmd[i])
+    {
         j = 0;
         while (cmd[i][j])
         {
-            if (ft_isdigit(cmd[i][0]))
+            if (ft_isdigit(cmd[i][0]) || !ft_isalpha(cmd[i][0]))
+            {
+                if (cmd[i][0] == '_')
+                    break;
+                ft_printf("bash: not a valid identifierss\n");
+                global_struct.g_exit_status = 1;
+                pipes_n_redirection->exit_builtin = 1;
+                flag = 1;
+                break;
+            }
+            if ((cmd[i][j] == '+' && cmd[i][j + 1] == '+'))
             {
                 ft_printf("bash: not a valid identifier\n");
                 global_struct.g_exit_status = 1;
@@ -297,20 +345,11 @@ void myexport(char **cmd, t_list *my_env, t_pipes_n_redirection *pipes_n_redirec
                 flag = 1;
                 break;
             }
-            if (cmd[i][j] == '+' && cmd[i][j + 1] == '+')
-            {
-                ft_printf("bash: not a valid identifier\n");
-                global_struct.g_exit_status = 1;
-                pipes_n_redirection->exit_builtin = 1;
-                flag = 1;
-                break;
-            }
-            else if ((cmd[i][j] == '+' && cmd[i][j + 1] == '=') || cmd[i][j] == '=')
+            else if ((cmd[i][j] == '+' && cmd[i][j + 1] == '=') || cmd[i][j] == '=' || cmd[i][j] == '+')
             {
                 if (!key)
                     key = ft_substr(cmd[i], 0, j);
                 value = ft_strchr_inc(cmd[i], '=');
-                // printf("%s=%s\n", key, value);
                 while (key[z])
                 {
                     if (!ft_isalpha(key[z]))
@@ -320,7 +359,7 @@ void myexport(char **cmd, t_list *my_env, t_pipes_n_redirection *pipes_n_redirec
                             ft_printf("bash: not a valid identifier\n");
                             free(key);
                             key = NULL;
-                           global_struct.g_exit_status = 1;
+                            global_struct.g_exit_status = 1;
                             pipes_n_redirection->exit_builtin = 1;
                             flag = 1;
                             break;
@@ -353,9 +392,9 @@ void my_export_add(char *cmd, t_pipes_n_redirection *pipes_n_redirection, t_list
 {
     int equal;
     int offset = 0;
-    char *key;
-    char *tmp_key;
-    char *value;
+    char *key = NULL;
+    char *value = NULL;
+    char *tmp_key = NULL;
     int j = 0;
     int lol = 0;
     t_list *current;
@@ -364,43 +403,41 @@ void my_export_add(char *cmd, t_pipes_n_redirection *pipes_n_redirection, t_list
     j = 0;
     equal = 1;
     current = my_env;
+
     while (cmd[j])
     {
         if ((cmd[j] == '=' || cmd[j] == '+') && equal)
         {
             offset = 1;
             equal = 0;
+            free(key);
+            free(value);
             tmp_key = ft_substr(cmd, 0, j);
             key = ft_strdup(tmp_key);
+            free(tmp_key);
             value = ft_strdup(ft_strchr(cmd, '='));
             while (current)
             {
                 if (ft_strncmp(current->key, key, ft_strlen(key) + 1) == 0)
                 {
-                    if (!current->value) //!(ft_strncmp(current->value, value, -1) == 0)
+                    if (!current->value)
                     {
-                        if (!current->value)
-                        {
-                            // free(current->value);
-                            current->value = ft_strdup(ft_strchr(cmd, '='));
-                            // printf("%p\n", current->value);
-                        }
-                        if (current->all)
-                        {
-                            // free(current->key);
-                            // free(current->value);
-                            // free(current->all);
-                        }
+                        free(value);
+                        value = ft_strchr(cmd, '=');
+                        current->value = ft_strdup(value);
                         current->all = ft_strjoin_no_free(key, current->value);
-                        // printf("%s\n", current->all);
+                        free(key);
                     }
                     else
                     {
-                        // free(current->value);
-                        current->value = ft_strdup(ft_strchr(cmd, '='));
+                        free(current->all);
+                        free(current->value);
+                        free(value);
+                        value = ft_strchr(cmd, '=');
+                        current->value = ft_strdup(value);
                         current->all = ft_strjoin_no_free(key, current->value);
+                        free(key);
                     }
-
                     lol = 1;
                     break;
                 }
@@ -409,15 +446,12 @@ void my_export_add(char *cmd, t_pipes_n_redirection *pipes_n_redirection, t_list
             if (!lol)
             {
                 new_export = malloc(sizeof(t_list));
-                new_export->key = key;
-                new_export->value = value;
-                // new_export->all = malloc(ft_strlen(key) + ft_strlen(value) + 1);
+                new_export->key = ft_strdup(key);
+                new_export->value = ft_strdup(value);
                 new_export->all = ft_strjoin_no_free(key, ft_strchr_inc(cmd, '='));
+                free(key);
+                free(value);
                 new_export->next = NULL;
-                // printf("key %s\n", new_export->key);
-                // printf("key %s\n", new_export->key);
-                // if (!(ft_strncmp(new_export->key, key, ft_strlen(key) + 1) == 0))
-                // printf("??\n");
                 ft_lstadd_back(&my_env, new_export);
             }
             lol = 0;
@@ -429,18 +463,15 @@ void my_export_add(char *cmd, t_pipes_n_redirection *pipes_n_redirection, t_list
         current = my_env;
         while (current)
         {
-            if (ft_compare(current->key, cmd) == 0)
+            if (ft_strncmp(current->key, cmd, ft_strlen(cmd)) == 0)
             {
                 offset = 1;
-            }
-            else
-            {
-                offset = 0;
                 break;
             }
+            offset = 0;
             current = current->next;
         }
-        if (offset)
+        if (!offset)
         {
             new_export = malloc(sizeof(t_list));
             new_export->key = ft_strdup(cmd);
@@ -451,7 +482,6 @@ void my_export_add(char *cmd, t_pipes_n_redirection *pipes_n_redirection, t_list
             offset = 0;
         }
     }
-    i++;
 }
 
 void my_unset(char **to_unset, t_list **my_env)
